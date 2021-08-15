5cde8083f8c6c15a6b70b9f4e10c230970a5ed0a
YeoJiSu <duwltn1301@naver.com>
2021-08-15 16:13:53 +0900

    :electric_plug:Label

---
+++ components/_Common/Form/Label/Label.Container.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import LabelPresenter from "./Label.Presenter";
+
+const LabelContainer = (): JSX.Element => {
+  return (
+    <>
+      <LabelPresenter />
+    </>
+  );
+};
+
+export default LabelContainer;
---
+++ components/_Common/Form/Label/Label.Presenter.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { LabelStyled } from "./Label.Styled";
+
+const LabelPresenter = (): JSX.Element => {
+  return (
+    <>
+      <LabelStyled />
+    </>
+  );
+};
+
+export default LabelPresenter;
