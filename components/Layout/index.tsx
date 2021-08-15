af811696a5eefc6c90e1c27a6cf7e4494afb2160
YeoJiSu <duwltn1301@naver.com>
2021-08-15 16:11:02 +0900

    :feet: + :smile: Layout

---
+++ components/Layout/index.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import FooterContainer from "./FooterComponent/Footer.Container";
+import HeaderContainer from "./HeaderComponent/Header.Container";
+
+
+interface Props {
+  children?: React.ReactNode;
+}
+const Layout = ({ children }: Props): JSX.Element => {
+  return (
+    <>
+      <HeaderContainer />
+      <span>{children}</span>
+      <FooterContainer />
+    </>
+  );
+};
+
+export default Layout;
