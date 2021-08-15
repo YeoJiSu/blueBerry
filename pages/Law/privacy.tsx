d46b481459e5fa304401a61f83041e0cfc80f617
YeoJiSu <duwltn1301@naver.com>
2021-08-15 18:38:26 +0900

    법적고지 페이지

---
+++ pages/Law/privacy.tsx
@@ -0,0 +1,14 @@
+import Layout from "components/Layout";
+import React from "react";
+
+const PrivacyPage = (): JSX.Element => {
+  return (
+    <Layout>
+      <main>
+        <span>개인정보처리방침</span>
+      </main>
+    </Layout>
+  );
+};
+
+export default PrivacyPage;
---
+++ pages/Law/terms.tsx
@@ -0,0 +1,14 @@
+import Layout from "components/Layout";
+import React from "react";
+
+const TermsPage = (): JSX.Element => {
+  return (
+    <Layout>
+      <main>
+        <span>이용약관</span>
+      </main>
+    </Layout>
+  );
+};
+
+export default TermsPage;
