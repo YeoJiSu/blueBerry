8cb112a9e76bf205fe123f67dfb755857ee2dcdb
YeoJiSu <duwltn1301@naver.com>
2021-08-17 01:03:25 +0900

    sunglass page 수정

--- pages/goods/sunglass/index.tsx
+++ pages/goods/sunglass/index.tsx
@@ -1,11 +1,46 @@
 import Layout from "components/Layout";
-import React from "react";
+import React, { useCallback, useEffect, useState } from "react";
+import axios, { AxiosResponse } from "axios";
 import { GoodsSS } from "../goods.style";
 const SunglassPage = (): JSX.Element => {
+  const [sunglass, sunglassList] = useState([]);
+  useEffect(() => {
+    axios
+      .get("https://strapi.kspark.link/sunglasses")
+      .then((response: AxiosResponse) => {
+        const sunglassNew = response.data; /*
+          .filter((value) => {
+            if (value.New === true) {
+              return true;
+            }
+          })
+          .map((value) => {
+            return value;
+          });*/
+        //console.log("glassNew", glassNew);
+        sunglassList(sunglassNew);
+      });
+  }, []);
   return (
     <Layout>
       <GoodsSS>
-        <span>선글라스 페이지 </span>
+        <div className={"title"}>SUNGLASS</div>
+        <div className={"goods"}>
+          {sunglass.map((value, index) => {
+            return (
+              <>
+                <div className={"goods_img"}>
+                  <img
+                    src={sunglass[index]?.img_url}
+                    alt={sunglass[index]?.goodsName}
+                  />
+                  <p>{sunglass[index]?.goodsName}</p>
+                  <p>{sunglass[index]?.price}원</p>
+                </div>
+              </>
+            );
+          })}
+        </div>
       </GoodsSS>
     </Layout>
   );
