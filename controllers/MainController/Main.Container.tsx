56fd4887b9666003601b51cb3fe9fc05b2ddc9f1
YeoJiSu <duwltn1301@naver.com>
2021-08-19 11:52:13 +0900

    add controller && progress

---
+++ controllers/MainController/Main.Container.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import MainPresenter from "./Main.Presenter";
+
+const MainContainer = () => {
+  return (
+    <>
+      <MainPresenter />
+    </>
+  );
+};
+
+export default MainContainer;
